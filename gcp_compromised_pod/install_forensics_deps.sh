bcdba6379314a927ed457310bf3b2c9e3a2a0325
Manas Bellani <manasmbellani@gmail.com>
2024-03-05 00:27:02 +1100

    Update install_forensics_deps.sh to update plaso-tools steps to not prompt on installation

--- gcp_compromised_pod/install_forensics_deps.sh
+++ gcp_compromised_pod/install_forensics_deps.sh
@@ -5,7 +5,7 @@ wget https://raw.githubusercontent.com/google/container-explorer/main/script/set
 sudo bash /tmp/setup.sh install
 
 echo "[*] Installing plaso-tools..."
-sudo add-apt-repository universe
-sudo add-apt-repository ppa:gift/stable
+sudo add-apt-repository -y universe
+sudo add-apt-repository -y ppa:gift/stable
 sudo apt-get -y update
 sudo apt-get -y install plaso-tools
