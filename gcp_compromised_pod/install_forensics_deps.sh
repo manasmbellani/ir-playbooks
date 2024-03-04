892419f2528fdee1390e66fad772d91e9290ad2b
Manas Bellani <manasmbellani@gmail.com>
2024-03-05 00:17:50 +1100

    Update install_forensics_deps.sh to install plaso-tools

--- gcp_compromised_pod/install_forensics_deps.sh
+++ gcp_compromised_pod/install_forensics_deps.sh
@@ -3,3 +3,9 @@
 echo "[*] Installing container-explorer..."
 wget https://raw.githubusercontent.com/google/container-explorer/main/script/setup.sh -o /tmp/setup.sh
 sudo bash /tmp/setup.sh install
+
+echo "[*] Installing plaso-tools..."
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:gift/stable
+sudo apt-get -y update
+sudo apt-get -y install plaso-tools
