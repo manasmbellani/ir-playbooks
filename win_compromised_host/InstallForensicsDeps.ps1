90278beaa4fdb89bbc361fab83c15e64dc015006
Manas Bellani <manasmbellani@gmail.com>
2024-03-21 08:40:20 +1100

    Update InstallForensicsDeps.ps1 to refer to INSTALL_LOCATION var

--- win_compromised_host/InstallForensicsDeps.ps1
+++ win_compromised_host/InstallForensicsDeps.ps1
@@ -9,6 +9,9 @@ Set-Variable SLEEP_TIME -Option Constant -Value 60
 Write-Host "[*] Configuring TLS settings for downloading files..."
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+# Installation location for our binaries
+$INSTALL_LOCATION = "C:\Users\Administrator\Desktop\opt"
+
 # Get the current working directory 
 $cwd = $(Get-Location | Select -ExpandProperty Path)
 
@@ -19,42 +22,42 @@ if (Test-Path -Path ".\setup.secrets.env") {
     Write-Host "[!] No setup.secrets.env file found"
 }
 
-if (-Not (Test-Path -Path "C:\Users\Administrator\Desktop\opt")) {
+if (-Not (Test-Path -Path "$INSTALL_LOCATION")) {
     Write-Host "[*] Creating new directory 'opt' in Desktop..."
     New-item -ItemType Directory -Path "C:\Users\Administrator\Desktop\opt"
 }
 
-if (-Not (Test-Path -Path "C:\Users\Administrator\Desktop\opt\python")) {
+if (-Not (Test-Path -Path "$INSTALL_LOCATION\python")) {
     Write-Host "[*] Making directory python..."
-    New-item -ItemType Directory -Path "C:\Users\Administrator\Desktop\opt\python"
+    New-item -ItemType Directory -Path "$INSTALL_LOCATION\python"
     
     Write-Host "[*] Downloading python..."
     $url="https://www.python.org/ftp/python/3.11.3/python-3.11.3-amd64.exe"
-    (New-Object System.Net.WebClient).DownloadFile($url, "C:\Users\Administrator\Desktop\opt\python\python.exe")
+    (New-Object System.Net.WebClient).DownloadFile($url, "$INSTALL_LOCATION\python\python.exe")
 
     Write-Host "[*] Installing python..."
-    $command = "C:\Users\Administrator\Desktop\opt\python\python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0"
+    $command = "$INSTALL_LOCATION\python\python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0"
     Invoke-Expression "& $command"
 
     Write-Host "[*] Sleeping for $SLEEP_TIME seconds until python is installed..."
     sleep "$SLEEP_TIME"
 
-    Write-Host "[*] Removing python.exe installer...."
-    #Remove-Item -Path "C:\Users\Administrator\Desktop\opt\python\python.exe"
+    #Write-Host "[*] Removing python.exe installer...."
+    #Remove-Item -Path "$INSTALL_LOCATION\python\python.exe"
 }
 
 
-if (-Not (Test-Path -Path "C:\Users\Administrator\Desktop\opt\WMIExplorer")) {
+if (-Not (Test-Path -Path "$INSTALL_LOCATION\WMIExplorer")) {
     Write-Host "[*] Making directory WMIExplorer..."
-    New-item -ItemType Directory -Path "C:\Users\Administrator\Desktop\opt\WMIExplorer"
+    New-item -ItemType Directory -Path "$INSTALL_LOCATION\WMIExplorer"
 
     Write-Host "[*] Downloading WMIExplorer..."
     $url =  "https://github.com/vinaypamnani/wmie2/releases/download/v2.0.0.2/WmiExplorer_2.0.0.2.zip"
-    (New-Object System.Net.WebClient).DownloadFile("$url", "C:\Users\Administrator\Desktop\opt\WMIExplorer\WMIExplorer.zip")
+    (New-Object System.Net.WebClient).DownloadFile("$url", "$INSTALL_LOCATION\WMIExplorer\WMIExplorer.zip")
 
     Write-Host "[*] Extracting WMIExplorer zip file..."
-    Expand-Archive -Path "C:\Users\Administrator\Desktop\opt\WMIExplorer\WMIExplorer.zip" -DestinationPath "C:\Users\Administrator\Desktop\opt\WMIExplorer"
+    Expand-Archive -Path "$INSTALL_LOCATION\WMIExplorer\WMIExplorer.zip" -DestinationPath "$INSTALL_LOCATION\WMIExplorer"
 
     Write-Host '[*] Removing WMIExplorer zip file...'
-    Remove-Item -Path "C:\Users\Administrator\Desktop\opt\WMIExplorer\WMIExplorer.zip"
+    Remove-Item -Path "$INSTALL_LOCATION\WMIExplorer\WMIExplorer.zip"
 }
