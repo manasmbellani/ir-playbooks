1ea389ff20243136f47b3bbdc1accc11ddfe143b
Manas Bellani <manasmbellani@gmail.com>
2024-04-24 08:36:53 +1000

    Update install_forensics_deps.sh to merge deps for win_compromised_host

--- gcp_compromised_pod/install_forensics_deps.sh
+++ gcp_compromised_pod/install_forensics_deps.sh
@@ -43,3 +43,39 @@ source venv/bin/activate
 python3 -m pip install -r requirements.txt
 python3 -m pip install -e .
 deactivate
+
+echo "[*] Install volatility2..."
+apt-get -y install python2.7
+git clone https://github.com/volatilityfoundation/volatility.git /opt/volatility
+cd /opt/volatility
+python3 -m virtualenv venv
+source venv/bin/activate
+python2.7 setup.py install
+deactivate
+
+echo "[*] Installing volatility3..."
+git clone https://github.com/volatilityfoundation/volatility3 /opt/volatility3
+cd /opt/volatility3
+python3 -m virtualenv venv
+source venv/bin/activate
+python3 setup.py install 
+deactivate
+
+echo "[*] Installing plyvel..."
+mkdir /opt/plyvel
+cd /opt/plyvel
+python3 -m virtualenv venv
+source venv/bin/activate
+python3 -m pip install plyvel
+deactivate
+
+echo "[*] Installing uploadserver..."
+mkdir /opt/uploadserver
+cd /opt/uploadserver
+python3 -m virtualenv venv
+source venv/bin/activate
+python3 -m pip install uploadserver
+deactivate
+
+echo "[*] Installing bulk-extractor..."
+apt-get -y install bulk-extractor
