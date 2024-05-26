5d3503511be999cadb4884c7b04791f059a765c4
Manas Bellani <manasmbellani@gmail.com>
2024-05-26 23:35:30 +1000

    Create Invoke-ComplianceSearch.ps1

---
+++ azure_compromised_account/Invoke-ComplianceSearch.ps1
@@ -0,0 +1,45 @@
+<#
+  .SYNOPSIS Script to create a compliance case and run specific search
+#>
+
+# ---------- User Inputs ----------
+# Add a case name here
+$CaseName = "Test Case 6"
+
+# Add the search name here
+$SearchName = "Test Search 6"
+
+# Enter the content search here 
+# Ref: https://learn.microsoft.com/en-us/purview/ediscovery-keyword-queries-and-search-conditions
+$ContentSearch = "from:abcxyz@gmail.com"
+
+# Enter an action name here for exported data
+$ActionName = "Test Action 6"
+# ---------- User Inputs ----------
+
+
+Write-Host "[*] Connect to Exchange Online..."
+Import-Module ExchangeOnlineManagement
+Connect-IPPSSession -UserPrincipalName
+
+Write-Host "[*] Creating a new Compliance case: $CaseName..."
+$case = New-ComplianceCase -Name $CaseName
+
+Write-Host "[*] Creating content search: $SearchName to be searched everywhere..."
+New-ComplianceSearch -Name $SearchName -Case $CaseName -ContentMatchQuery $ContentSearch -ExchangeLocation All -SharePointLocation All -PublicFolderLocation All
+
+Write-Host "[*] Initiating search: $SearchName in case: $CaseName..."
+Start-ComplianceSearch -Identity $SearchName
+
+Write-Host "[*] Waiting for compliance search: $SearchName in case: $CaseName is completed..."
+$SearchStatus = ""
+while ($SearchStatus -ne "Completed") {
+  Sleep 2
+  
+  Write-Host "[*] Checking compliance search: $SearchName in case: $CaseName status..."
+  $SearchStatus = (Get-ComplianceSearch -Case $CaseName -Identity $SearchName).Status
+  Write-host "[*] Search Status: $SearchStatus"
+}
+
+Write-Host "[*] Initiating action: $ActionName for exporting Search data for search: $SearchName..."
+New-ComplianceSearchAction -ActionName $ActionName -SearchName $SearchName -Export -Confirm:$false -Force 
