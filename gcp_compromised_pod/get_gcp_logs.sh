d4d65b05ba3941115a0985085b588a4d87f7c6da
Manas Bellani <manasmbellani@gmail.com>
2024-06-08 21:41:27 +1000

    Update get_gcp_logs.sh

--- gcp_compromised_pod/get_gcp_logs.sh
+++ gcp_compromised_pod/get_gcp_logs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-HEADER_FILE="timestamp,"
+HEADER_CSV="timestamp,raw"
 USAGE="
 Summary:
   Script to download the GCP Logs in CSV format with useful fields pre-extracted
@@ -20,15 +20,22 @@ Examples:
     ./get_gcp_logs.sh
 "
 
+echo "[*] Creating output file: $OUTFILE_CSV if it exists..."
+echo "$HEADER_CSV" > "$OUTFILE_CSV"
+
 echo "[*] Extracting logs for logging query: $LOGGING_QUERY for the period: $START_DATE - $END_DATE..."
 #logging_query='timestamp >= "2024-06-07T00:00:00Z" AND timestamp <= "2024-06-08T23:59:59Z" AND protoPayload.serviceName="dns.googleapis.com" AND protoPayload.methodName="dns.changes.create"'
 gcloud logging read  "$LOGGING_QUERY" --format json > "$OUTFILE_JSON"
 
 echo "[*] Counting number of logs obtained..."
 num_logs=$(jq -r ". | length" "$OUTFILE_JSON")
+echo "[*] $num_logs GCP Logs downloaded"
 
 echo "[*] Parsing the logs downloaded via 'jq'..."
-
-
-
-
+IFS=$'\n'
+for i in $(seq 0 $num_logs); do
+  log=$(jq -r ".[$i]" "$OUTFILE_JSON")
+  timestamp=$(echo "$log" | jq -r ".timestamp")
+  raw_log=$(echo "$log" | tr -s '\"' "'" | tr -d "\n\r")
+  echo "\"$timestamp\",\"$raw_log\"" >> "$OUTFILE_CSV"
+done
