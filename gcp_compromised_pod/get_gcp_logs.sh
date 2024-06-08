911b5a286415496fe65f6d5e5222ce27f8197c60
Manas Bellani <manasmbellani@gmail.com>
2024-06-09 09:35:22 +1000

    Update get_gcp_logs.sh

--- gcp_compromised_pod/get_gcp_logs.sh
+++ gcp_compromised_pod/get_gcp_logs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-HEADER_CSV="timestamp,raw"
+HEADER_CSV="timestamp,log_type,severity,payload_type,principal_email,service_name,method_name,project_id,resource_name,text_payload,raw"
+LOG_TYPE="gcp"
 USAGE="
 Summary:
   Script to download the GCP Logs in CSV format with useful fields pre-extracted
@@ -12,11 +13,11 @@ Examples:
     CLOUDSDK_CORE_PROJECT=citric-snow-362912 \
     CLOUDSDK_COMPUTE_REGION=us-central1 \
     CLOUDSDK_COMPUTE_ZONE=us-central1-c \
-    LOGGING_QUERY='protoPayload.serviceName=\"dns.googleapis.com\" AND protoPayload.methodName=\"dns.changes.create\"' \
+    LOGGING_QUERY='protoPayload.serviceName="dns.googleapis.com" AND protoPayload.methodName="dns.changes.create"' \
     OUTFILE_CSV=out-gcp-logs.csv \
     OUTFILE_JSON=out-gcp-logs.json \
-    START_DATE=\"2024-06-07\" \
-    END_DATE=\"2024-06-08\" \
+    START_DATE="2024-06-07" \
+    END_DATE="2024-06-08" \
     ./get_gcp_logs.sh
 "
 
@@ -24,8 +25,11 @@ echo "[*] Creating output file: $OUTFILE_CSV if it exists..."
 echo "$HEADER_CSV" > "$OUTFILE_CSV"
 
 echo "[*] Extracting logs for logging query: $LOGGING_QUERY for the period: $START_DATE - $END_DATE..."
-#logging_query='timestamp >= "2024-06-07T00:00:00Z" AND timestamp <= "2024-06-08T23:59:59Z" AND protoPayload.serviceName="dns.googleapis.com" AND protoPayload.methodName="dns.changes.create"'
-gcloud logging read  "$LOGGING_QUERY" --format json > "$OUTFILE_JSON"
+logging_query="timestamp >= \"$START_DATE""T00:00:00Z\" AND timestamp <= \"$END_DATE""T00:00:00Z\""
+if [ ! -z "$LOGGING_QUERY" ]; then
+    logging_query="$logging_query AND $LOGGING_QUERY"
+fi
+gcloud logging read  "$logging_query" --format json > "$OUTFILE_JSON"
 
 echo "[*] Counting number of logs obtained..."
 num_logs=$(jq -r ". | length" "$OUTFILE_JSON")
@@ -33,9 +37,19 @@ echo "[*] $num_logs GCP Logs downloaded"
 
 echo "[*] Parsing the logs downloaded via 'jq'..."
 IFS=$'\n'
-for i in $(seq 0 $num_logs); do
+for i in $(seq 0 $(($num_logs-1)) ); do
+  echo "[*] Parsing log: $i via 'jq' and writing to file: $OUTFILE_CSV..."
   log=$(jq -r ".[$i]" "$OUTFILE_JSON")
   timestamp=$(echo "$log" | jq -r ".timestamp")
-  raw_log=$(echo "$log" | tr -s '\"' "'" | tr -d "\n\r")
-  echo "\"$timestamp\",\"$raw_log\"" >> "$OUTFILE_CSV"
+  log_name=$(echo "$log" | jq -r ".logName")
+  severity=$(echo "$log" | jq -r ".severity")
+  payload_type=$(echo "$log" | jq -r ".protoPayload.\"@type\"")
+  principal_email=$(echo "$log" | jq -r ".protoPayload.authenticationInfo.principalEmail" )
+  service_name=$(echo "$log" | jq -r ".protoPayload.serviceName")
+  method_name=$(echo "$log" | jq -r ".protoPayload.methodName")
+  project_id=$(echo "$log" | jq -r ".resource.labels.project_id")
+  resource_name=$(echo "$log" | jq -r ".protoPayload.resourceName")
+  text_payload=$(echo "$log" | jq -r ".textPayload")
+  raw=$(echo "$log" | tr -s '\"' "'" | tr -d "\n\r")
+  echo "\"$timestamp\",\"$LOG_TYPE\",\"$severity\",\"$payload_type\",\"$principal_email\",\"$service_name\",\"$method_name\",\"$project_id\",\"$resource_name\",\"$text_payload\",\"$raw\"" >> "$OUTFILE_CSV"
 done
